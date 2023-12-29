664b37381eac92e528e0d9e8244f6780a2dc4710
your_name <catgeot@naver.com>
2023-12-29 18:23:50 +0900

    비디오 통합작업

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
-import Main from '../components/section/Main'
 
+import Main from '../components/section/Main'
 import Today from '../components/contents/Today'
 import Developer from '../components/contents/Developer'
-import Webd from '../components/contents/Webd'
-import Website from '../components/contents/Website'
-import Gsap from '../components/contents/Gsap'
-import Portfolio from '../components/contents/Portfolio'
-import Youtube from '../components/contents/Youtube'
+import VideoSlider from '../components/video/VideoSlider'
+
+import { webdText } from '../data/webd'
+import { websiteText } from '../data/website'
+import { gsapText } from '../data/gsap'
+import { portfolioText } from '../data/portfolio'
+import { youtubeText } from '../data/youtube'
+
 
 const Home = () => {
     return (
@@ -17,11 +20,11 @@ const Home = () => {
         >
             <Today />
             <Developer />
-            <Webd />
-            <Website />
-            <Gsap />
-            <Portfolio />
-            <Youtube />
+            <VideoSlider videos={webdText} title='WebDesign' id='webd' />
+            <VideoSlider videos={ websiteText } title='Website' id='website' />
+            <VideoSlider videos={gsapText} title='Gsap' id='gsap' />
+            <VideoSlider videos={portfolioText} title='Portfolio' id='portfolio' />
+            <VideoSlider videos={youtubeText} title='youtube' id='youtube' />
         </Main>
     )
 }
